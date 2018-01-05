72c7a830c77d293bae7ff0d8a1331733effdeea4
jfmatthews90 <jfmatthews90@gmail.com>
2018-01-05 16:06:16 -0500

    Updated routes and controller for form. Created SendGrid for Heroku emails.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+	root 'simple_pages#index'
+
   get 'simple_pages/about'
 
   get 'simple_pages/contact'
 
   get 'simple_pages/index'
 
-  root 'simple_pages#index'
+  #get 'simple_pages/thank_you'
+
+  post 'simple_pages/thank_you'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
